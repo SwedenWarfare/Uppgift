ce4b73efd65a95dde1dd9628ab547e6510be0a9d
elev <elev@192.168.137.1>
2020-11-06 12:13:06 +0100

    Fixed Some thing

--- Uppgift/src/me/jerk/bil/BilGui.java
+++ Uppgift/src/me/jerk/bil/BilGui.java
@@ -2,6 +2,10 @@ package me.jerk.bil;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,15 +14,19 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import me.jerk.uppgift.MainMenu;
 import me.jerk.utils.GlobalVars;
 
-public class BilGui {
+public class BilGui implements ActionListener {
+	List<BilInfo> bilInfoList = new ArrayList<>();
 	JPanel bilPanel = new JPanel();
+	static int index = 0;
 	JFrame frame = GlobalVars.mainFrame;
 	JButton backButton = new JButton("Main Menu");
 	JButton prevButton = new JButton("Prev index");
 	JButton nextButton = new JButton("Next index");
 	JButton saveButton = new JButton("Submit");
+	JButton printOutButton;
 	JTextArea inputArea;
 	JTextArea outPutArea;
 	JScrollPane outscrollPane;
@@ -27,10 +35,16 @@ public class BilGui {
 	JLabel inputLabel = new JLabel("Input");
 	JLabel outPutLabel = new JLabel("Output");
 	public BilGui() {
+		index = bilInfoList.size();
+		printOutButton = new JButton("Print "+index);
 		inputArea = new JTextArea(10,10);
 		inscrollPane = new JScrollPane(inputArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		
+		backButton.addActionListener(this);
+		prevButton.addActionListener(this);
+		nextButton.addActionListener(this);
+		saveButton.addActionListener(this);
 		outPutArea = new JTextArea(10,10);
+	
 		outscrollPane = new JScrollPane(outPutArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
 		GridBagLayout frameLayout = new GridBagLayout();
 		GridBagConstraints frameGbc = new GridBagConstraints();
@@ -86,5 +100,26 @@ public class BilGui {
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == backButton)
+		{
+			frame.remove(bilPanel);
+			frame.remove(outPutLabel);
+			frame.remove(inputLabel);
+			frame.remove(inscrollPane);
+			frame.remove(outscrollPane);
+			new MainMenu();
+		}
+		else if(e.getSource() == prevButton) {
+			
+		}
+		else if(e.getSource() == nextButton) {
+			
+		}
+		else if(e.getSource() == saveButton) {
+			
+		}
 	}
 }
